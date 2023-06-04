0e7dafae636a5608bcc5106347be8b5a36d5daaa
gamer-does-micro-bit <135429281+gamer-does-micro-bit@users.noreply.github.com>
2023-06-04 08:59:12 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,50 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # # # .
+        # # . # #
+        # . . . #
+        # # . # #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # # . # #
+        # . # . #
+        # # . # #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # . # #
+        # # # # #
+        . # # # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # # # .
+        . # # # .
+        . # # # .
+        # . . . #
+        `)
+})
+basic.forever(function () {
+    music.playMelody("C5 - A A B B A E ", 200)
+    music.playMelody("C5 - A B B B A E ", 200)
+    music.playMelody("C5 - A A B B A E ", 200)
+    music.playMelody("C5 - A B B B A E ", 170)
+})
